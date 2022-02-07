cfa2c75398099614f8b696670496a6999feaf0e1
FaheemAKamal <83312364+FaheemAKamal@users.noreply.github.com>
2022-02-07 14:02:37 -0500

    Create RobotBoundedInCircle.java

---
+++ RobotBoundedInCircle.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean isRobotBounded(String instructions) {
+        if (instructions.length() == 0)
+            return false;
+        int x = 0;
+        int y = 0;  // initial points of the robot
+        String directions = "North"; // initial direction of robot
+        for (char ch: instructions.toCharArray()) {
+            if (ch == 'G') {
+                if (directions.equals("North"))
+                    y += 1;
+                else if (directions.equals("South"))
+                    y -= 1;
+                else if(directions.equals("East"))
+                    x += 1;
+                else
+                    x -= 1;
+            }
+            else if (ch == 'L') {
+                if (directions.equals("North"))
+                    directions = "West";
+                else if (directions.equals("West"))
+                    directions = "South";
+                else if (directions.equals("South"))
+                    directions = "East";
+                else directions = "North";
+            }
+            else if (ch == 'R') {
+                if (directions.equals("North"))
+                    directions = "East";
+                else if (directions.equals("East"))
+                    directions = "South";
+                else if (directions.equals("South"))
+                    directions = "West";
+                else directions = "North";
+            }
+        }
+        if (x == 0 && y == 0) return true;
+        if(directions.equals("North")) return false;
+        return true;
+    }
+}
